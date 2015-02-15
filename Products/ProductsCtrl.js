8e5c1e918030cc69e73990a97989c7ab37aa859a
Tim Garlick <timgarlick11@gmail.com>
2015-02-15 00:55:09 -0700

    almost done

--- Products/ProductsCtrl.js
+++ Products/ProductsCtrl.js
@@ -2,8 +2,9 @@ var app = angular.module('miniRouting')
 
 app.controller('productCtrl', function($scope, $routeParams, productService) {
 	
-	if ($routeParams.id === 'home') {
+	if ($routeParams.id === 'shoes') {
 		$scope.productData === productService.shoeData;
+		console.log(productService.shoeData)
 
 		} else if ( $routeParams.id === 'socks') {
 			$scope.productData === productService.sockData;
